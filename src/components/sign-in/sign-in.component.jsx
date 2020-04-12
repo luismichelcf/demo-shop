a1550c4b48551b21add1e1100ed81e634250ee7b
INTERPRIKA <INTERPRIKA@INTERPRIKAs-MacBook-Pro.local>
2020-04-12 18:28:28 -0500

    added sign in form

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import React from "react"
+import FOrmInput from "../../components/form-input/form-input.component"
+
+import "./sign-in.styles.scss"
+import FormInput from "../../components/form-input/form-input.component"
+
+class SignIn extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			email: "",
+			password: "",
+		}
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+
+		this.setState({ email: "", password: "" })
+	}
+
+	handleChange = (event) => {
+		const { value, name } = event.target
+
+		this.setState({ [name]: value })
+	}
+
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>I alrady have an account</h2>
+				<span>Sign in with your email and password</span>
+
+				<form onSubmit={this.handleSubmit}>
+					<FormInput
+						name="email"
+						type="email"
+						label="email"
+						handleChange={this.handleChange}
+						value={this.state.email}
+						required
+					/>
+					<FormInput
+						name="password"
+						type="password"
+						value={this.state.password}
+						handleChange={this.handleChange}
+						label="password"
+						required
+					/>
+
+					<input type="submit" value="Submit Form" />
+				</form>
+			</div>
+		)
+	}
+}
+
+export default SignIn
